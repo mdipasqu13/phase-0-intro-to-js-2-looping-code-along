d74fad79662665e80ea5c687a1c0fb68e3366496
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-02-02 19:05:36 -0500

    completed loops lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let newCards = [];
+function writeCards(cards, string) {
+  for (let i = 0; i < cards.length; i++) {
+    newCards.push(`Thank you, ${cards[i]}, for the wonderful ${string} gift!`);
+  }
+return(newCards);
+}
+writeCards(cards, "birthday");
+console.log(newCards);
+
+let n = 10
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n--);
+  }
+};
+countDown(n);
